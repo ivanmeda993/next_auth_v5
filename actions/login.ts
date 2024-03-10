37417346934e2e8d0d4bf35c5eebe85e2ddf2da0
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2024-03-10 17:55:26 +0100

    Register Form

---
+++ actions/login.ts
@@ -0,0 +1,16 @@
+"use server";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+export const login = async (
+  values: z.infer<typeof LoginSchema>,
+  callbackUrl?: string | null,
+) => {
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  console.log("login", values);
+  return { success: "Logged in" };
+};
