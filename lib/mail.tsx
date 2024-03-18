c28c0a064d40dc511fdb7b2348418bbd2e9d6ff3
ivanmilicevic <93290646+ivanmeda993@users.noreply.github.com>
2024-03-18 16:19:02 +0100

    Reset password

--- lib/mail.tsx
+++ lib/mail.tsx
@@ -1,5 +1,6 @@
 import { Resend } from "resend";
 import ConfirmEmail from "@/components/emails/confirm";
+import ResetPasswordEmail from "@/components/emails/reset-password";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -11,5 +12,18 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     to: email,
     subject: "Confirm your email address",
     react: <ConfirmEmail validationCode={confirmationLink} />,
+  });
+};
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `${process.env.NEXTAUTH_URL}/auth/new-password?token=${token}`;
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Reset your password",
+    react: (
+      <ResetPasswordEmail userFirstname={email} resetPasswordLink={resetLink} />
+    ),
   });
 };
