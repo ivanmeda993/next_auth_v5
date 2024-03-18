757646aea1017609cfc0c4ba9fc354c41b870d6b
ivanmilicevic <93290646+ivanmeda993@users.noreply.github.com>
2024-03-18 18:58:35 +0100

    Server & Client example

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,4 +1,5 @@
 export const ROUTES = [
+  { path: "/server", name: "Server" },
   { path: "/clients", name: "Clients" },
   { path: "/admin", name: "Admin" },
   { path: "/settings", name: "Settings" },
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,5 @@
 import { UserRole } from "@prisma/client";
-import NextAuth, { type DefaultSession } from "next-auth";
+import { type DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
@@ -16,5 +16,7 @@ declare module "next-auth" {
 declare module "@auth/core/jwt" {
   interface JWT {
     role: UserRole;
+    isTwoFactorEnabled: boolean;
+    isOAuth: boolean;
   }
 }
